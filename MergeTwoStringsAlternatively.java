d193539c73f306843e44ecbca2f1c48c76b01402
Sandhiya R <sandhiyarajkumar0305@gmail.com>
2026-09-16 12:00:13 +0530

    Refactor mergeString method to use StringBuilder for improved performance and clarity

--- MergeTwoStringsAlternatively.java
+++ MergeTwoStringsAlternatively.java
@@ -1,21 +1,41 @@
 public class MergeTwoStringsAlternatively {
 
     public static String mergeString(String str1, String str2) {
-        int i1 = 1, i2 = 0, l1 = str1.length(), l2 = str2.length();
-        String merged = String.valueOf(str1.charAt(0));
+
+        //Approach 1
+
+        // int i1 = 1, i2 = 0, l1 = str1.length(), l2 = str2.length();
+        // String merged = String.valueOf(str1.charAt(0));
+        // while(i1 < l1 && i2 < l2) {
+        //     if(i2 < i1) {
+        //         merged += str2.charAt(i2);
+        //         i2++;
+        //     }
+        //     else{
+        //         merged += str1.charAt(i1);
+        //         i1++;
+        //     }
+        // }
+        // if(i1 < l1) merged += str1.substring(i1, l1);
+        // else if(i2 < l2) merged += str2.substring(i2, l2);
+        // return merged;
+
+        //Approach 2
+
+        StringBuilder merged = new StringBuilder();
+        
+        int i1 = 0, i2 = 0, l1 = str1.length(), l2 = str2.length();
+
         while(i1 < l1 && i2 < l2) {
-            if(i2 < i1) {
-                merged += str2.charAt(i2);
-                i2++;
-            }
-            else{
-                merged += str1.charAt(i1);
-                i1++;
-            }
+            merged.append(str1.charAt(i1++));
+            merged.append(str2.charAt(i2++));
         }
-        if(i1 < l1) merged += str1.substring(i1, l1);
-        else if(i2 < l2) merged += str2.substring(i2, l2);
-        return merged;
+
+        while(i1 < l1) merged.append(str1.charAt(i1++));
+        while(i2 < l2) merged.append(str2.charAt(i2++));
+
+        return merged.toString();
+
     }
     public static void main(String[] args) {
         System.out.println(mergeString("abc", "pq"));
