1d7cfdf9eb429167aeefeb98fde27ef5616aa954
Sandhiya R <sandhiyarajkumar0305@gmail.com>
2026-09-16 13:33:30 +0530

    Add MaximumMoons class to calculate the maximum number of moons based on user input

---
+++ MaximumMoons.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MaximumMoons {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Suns: ");
+        int n = s.nextInt();
+        s.nextLine();
+        System.out.print("Planets: ");
+        int m = s.nextInt();
+        s.nextLine();
+        int max = 0, rowSum = 0;
+        int[][] solar = new int[n][m];
+        for(int i = 0; i < n; i++) {
+            rowSum = 0;
+            for(int j = 0; j < m; j++) {
+                solar[i][j] = s.nextInt();
+                rowSum += solar[i][j];
+            }
+            max = (rowSum < max) ? max : rowSum;
+            s.nextLine();
+        }
+        System.out.println("Maximum number of moons: " + max);
+    }
+}
