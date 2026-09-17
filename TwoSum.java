7478b692a2d44f18bc91dae7014369916dabbbeb
Sandhiya R <sandhiyarajkumar0305@gmail.com>
2026-09-17 11:45:47 +0530

    Add TwoSum class to find indices of two numbers that add up to a target

---
+++ TwoSum.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class TwoSum {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(List.of(2, 7, 11, 15));
+        int target = 9, flag = 0;
+        for(int i = 0; i < arr.size(); i++) {
+            int num = target - arr.get(i);
+            if(arr.contains(num)) {
+                System.out.println(i + " " + arr.indexOf(num));
+                flag = 1;
+                break;
+            }
+        }
+        if(flag != 1) System.out.println(-1);
+    }
+}
+
