be7d89604eb868f6d4eecf5f8eed01bc6f2f2208
Sandhiya R <sandhiyarajkumar0305@gmail.com>
2026-09-17 11:54:45 +0530

    Fix TwoSum logic to ensure distinct indices for the two numbers

--- TwoSum.java
+++ TwoSum.java
@@ -9,7 +9,7 @@ public class TwoSum {
         int target = 9, flag = 0;
         for(int i = 0; i < arr.size(); i++) {
             int num = target - arr.get(i);
-            if(arr.contains(num)) {
+            if(arr.contains(num) && arr.indexOf(num) != i) {
                 System.out.println(i + " " + arr.indexOf(num));
                 flag = 1;
                 break;
